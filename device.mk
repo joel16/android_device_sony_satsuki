fa3086066c97b6d3d15ec7a45313f4d730e95eba
Joel16 <joel16@iastate.edu>
2019-12-16 21:03:15 -0500

    Fix fingerprint with android hardware biometrics

--- device.mk
+++ device.mk
@@ -75,4 +75,4 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    fingerprintd
+    android.hardware.biometrics.fingerprint@2.1-service
