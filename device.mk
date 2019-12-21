d731ebade1801a368eae8aa13c809fce9cdf9edc
Joel16 <joel16@iastate.edu>
2019-12-21 09:04:56 -0500

    Fix sensors and revert back to using thermal-engine.conf

--- device.mk
+++ device.mk
@@ -53,8 +53,7 @@ PRODUCT_COPY_FILES += \
 
 # IRSC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
 # NFC configs
 PRODUCT_COPY_FILES += \
