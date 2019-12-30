cd9e31bbb0d27d0d7c9a93062ab2b8f1c9afa81a
Joel16 <joel16@iastate.edu>
2019-12-29 21:28:44 -0500

    Clean up lineage makefiles

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
-#
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,11 +12,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-$(call inherit-product, device/sony/satsuki/full_satsuki.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff
+# Inherit from satsuki device
+$(call inherit-product, device/sony/satsuki/device.mk)
+
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_satsuki
+PRODUCT_DEVICE := satsuki
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Xperia Z5 Premium
+
+PRODUCT_GMS_CLIENTID_BASE := android-sony
+
+TARGET_VENDOR := Sony
+TARGET_VENDOR_PRODUCT_NAME := satsuki
