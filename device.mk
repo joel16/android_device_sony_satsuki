76f1fac36ef33bcbe96c5c26e600f636be63135d
Joel <joel16@iastate.edu>
2020-05-08 22:28:51 -0400

    satsuki: Use android.hardware.biometrics.fingerprint@2.1-service

--- device.mk
+++ device.mk
@@ -76,4 +76,4 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    fingerprintd
+    android.hardware.biometrics.fingerprint@2.1-service
